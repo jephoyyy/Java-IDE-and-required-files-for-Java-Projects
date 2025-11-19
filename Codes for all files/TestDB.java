c769830d8ff88b17b303c6165307a8189a5bb1d4
Jeff <jeffersonvillapando131@gmail.com>
2025-11-19 15:04:28 +0800

    TestDB.java

--- Codes for all files/TestDB.java
+++ Codes for all files/TestDB.java
@@ -1,12 +1,12 @@
 import java.sql.*;
-
+// File for testing if the database is connected to the main java.
 public class TestDB {
     public static void main(String[] args) {
         try (Connection conn = Database.connect()) {
             if (conn != null) {
                 System.out.println("Yayyyy!, Database connected successfully!");
             }
-        } catch (SQLException e) {
+        } catch (SQLException e) { // Pag nag ka error sa database mag sshow yung message
             System.out.println("Connection failed: " + e.getMessage());
         }
     }
