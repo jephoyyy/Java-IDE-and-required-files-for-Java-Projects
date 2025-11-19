7a9e18d32b1b848f075da7c95fdbf910adc07a4e
Jeff <jeffersonvillapando131@gmail.com>
2025-11-19 14:59:40 +0800

    Database.java

--- Codes for all files/Database.java
+++ Codes for all files/Database.java
@@ -1,10 +1,10 @@
 import java.sql.*;
 
 public class Database {
-    // Full path ng database (palitan kung iba ang location mo)
+    // destination or address ng database sa file.
     private static final String DB_URL = "jdbc:sqlite:C:/Java Project/canteen.db";
 
-    // Method para mag connect sa database
+    // method para i-connect yung database sa main system
     public static Connection connect() throws SQLException {
         return DriverManager.getConnection(DB_URL);
     }
