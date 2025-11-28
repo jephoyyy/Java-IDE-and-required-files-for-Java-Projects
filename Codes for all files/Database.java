71ed6e0f0bee0f60f674e70f56c6cf6d7cbfe79d
Jeff <jeffersonvillapando131@gmail.com>
2025-11-27 23:05:58 -1200

    Update Database.java

--- Codes for all files/Database.java
+++ Codes for all files/Database.java
@@ -1,11 +1,11 @@
-import java.sql.*;
+import java.sql.*; //import all the important classes of java.sql
 
 public class Database {
     // destination or address ng database sa file.
     private static final String DB_URL = "jdbc:sqlite:C:/Java Project/canteen.db";
 
     // method para i-connect yung database sa main system
-    public static Connection connect() throws SQLException {
+    public static Connection connect() throws SQLException { // para i handle yuhg error sa database.
         return DriverManager.getConnection(DB_URL);
     }
 }
